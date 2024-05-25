efcb5323d1aa6bb7d6e7e67f3eb0ee3fe55eac13
MissSunflower25 <153073194+MissSunflower25@users.noreply.github.com>
2024-05-25 16:07:14 +0200

    Made the js code more efficient by avoiding removing repetitive code and making use of conditional if statementsand a for loop.

--- index.js
+++ index.js
@@ -6,42 +6,20 @@ const password1 = document.getElementById("password-1");
 const password2 = document.getElementById("password-2");
 
 
-btnEl.onclick = function btnClick (){
-     let a =  Math.floor(Math.random() * characters.length)
-     let b = Math.floor(Math.random() * characters.length) 
-     let c =  Math.floor(Math.random() * characters.length)
-     let d =  Math.floor(Math.random() * characters.length)
-     let e =  Math.floor(Math.random() * characters.length)
-     let f =  Math.floor(Math.random() * characters.length)
-     let g =  Math.floor(Math.random() * characters.length)
-     let h =  Math.floor(Math.random() * characters.length)
-     let i =  Math.floor(Math.random() * characters.length)
-     let j =  Math.floor(Math.random() * characters.length)
-     let k =  Math.floor(Math.random() * characters.length)
-     let l =  Math.floor(Math.random() * characters.length)
-     let m =  Math.floor(Math.random() * characters.length)
-     let n =  Math.floor(Math.random() * characters.length)
-     let o =  Math.floor(Math.random() * characters.length)
-    
-     password1.textContent = characters[a] + characters[b] + characters[c] + characters[d] + characters[e] + characters[f] + characters[g] + characters[h] + characters[i] + characters[j] + characters[k] + characters[l] + characters[m] + characters[n] + characters[o]
+btnEl.addEventListener("click", ()=>{
+     let password1Storage = [];
+     let password2Storage = [];
 
-     let p =  Math.floor(Math.random() * characters.length)
-     let q = Math.floor(Math.random() * characters.length) 
-     let r =  Math.floor(Math.random() * characters.length)
-     let s =  Math.floor(Math.random() * characters.length)
-     let t =  Math.floor(Math.random() * characters.length)
-     let v =  Math.floor(Math.random() * characters.length)
-     let w =  Math.floor(Math.random() * characters.length)
-     let x =  Math.floor(Math.random() * characters.length)
-     let y =  Math.floor(Math.random() * characters.length)
-     let z =  Math.floor(Math.random() * characters.length)
-     let aa =  Math.floor(Math.random() * characters.length)
-     let bb =  Math.floor(Math.random() * characters.length)
-     let cc =  Math.floor(Math.random() * characters.length)
-     let dd =  Math.floor(Math.random() * characters.length)
-     let ee =  Math.floor(Math.random() * characters.length)
+for (let i=0; i < 30; i++){
+     
+     if (password1Storage.length < 15){
+          password1Storage += characters[Math.floor(Math.random()*characters.length)]
+     }else{
+          password2Storage += characters[Math.floor(Math.random()*characters.length)]
+     }
 
-     password2.textContent = characters[p] + characters[q] + characters[r] + characters[s] + characters[t] + characters[v] + characters[w] + characters[x] + characters[y] + characters[z] + characters[aa] + characters[bb] + characters[cc] + characters[dd] + characters[ee]
+//     console.log(password1Storage)
+password1.textContent = password1Storage;
+password2.textContent = password2Storage;
 
-}
-
+}})
